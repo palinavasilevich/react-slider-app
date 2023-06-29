d6bf61141d55db2b778d975278b8f4c191fc10c4
PolinaVasilevich <polina.vasilevichh@gmail.com>
2023-06-29 15:31:45 +0200

    created a carousel using the library react-slick

---
+++ src/components/SlickCarousel.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Slider from "react-slick";
+import { FaQuoteRight } from "react-icons/fa";
+
+import { list, longList } from "../data";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const SlickCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+  };
+
+  return (
+    <div className="slick-container">
+      <Slider {...settings}>
+        {list.map((person) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <div key={id} className="slick-slide">
+              <img src={image} alt={name} className="person-img" />
+              <h5 className="name">{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default SlickCarousel;
